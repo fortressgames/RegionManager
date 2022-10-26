fd001301683d2192b21d028d06c1731f1c49a4d7
FortressGames <fortressgames1@gmail.com>
2022-10-26 20:10:32 +0100

    fix density

--- src/main/java/net/fortressgames/regionmanager/regions/DisplayParticleTask.java
+++ src/main/java/net/fortressgames/regionmanager/regions/DisplayParticleTask.java
@@ -13,7 +13,6 @@ import java.util.List;
 public class DisplayParticleTask extends FortressRunnable {
 
 	private final Particle particle;
-	@Setter private int density;
 
 	private final Region region;
 	private final List<Vector3> points = new ArrayList<>();
@@ -21,7 +20,6 @@ public class DisplayParticleTask extends FortressRunnable {
 	public DisplayParticleTask(Particle particle, int density, Region region) {
 		this.particle = particle;
 		this.region = region;
-		this.density = density;
 
 		int play = 0;
 		List<Vector3> list = region.getAllPoints();
@@ -29,7 +27,7 @@ public class DisplayParticleTask extends FortressRunnable {
 
 		for(Vector3 point : list) {
 
-			if(play == 30) {
+			if(play == density) {
 				points.add(point);
 				play = 0;
 				continue;
@@ -43,6 +41,6 @@ public class DisplayParticleTask extends FortressRunnable {
 	public void run() {
 
 		points.forEach(vector3 -> region.getWorld().spawnParticle(particle,
-				new Location(region.getWorld(), vector3.getBlockX(), vector3.getBlockY(), vector3.getBlockZ()), density, 0, 0, 0, 0));
+				new Location(region.getWorld(), vector3.getBlockX(), vector3.getBlockY(), vector3.getBlockZ()), 1, 0, 0, 0, 0));
 	}
 }
